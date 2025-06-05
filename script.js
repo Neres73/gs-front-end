792185c98942b10e7e37d014b2e2fed2afdc03f6
Neres73 <guganeres398@gmail.com>
2025-06-05 17:10:16 -0300

    js-adicionado

--- script.js
+++ script.js
@@ -2,15 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.querySelector('.hamburger');
     const navLinks = document.querySelector('.nav-links');
 
-    hamburger.addEventListener('click', function() {
-        navLinks.classList.toggle('active');
-    });
-
-    navLinks.querySelectorAll('a').forEach(link => {
-        link.addEventListener('click', () => {
-            if (navLinks.classList.contains('active')) {
-                navLinks.classList.remove('active');
-            }
+    if (hamburger && navLinks) {
+        hamburger.addEventListener('click', function() {
+            navLinks.classList.toggle('active');
         });
-    });
+    }
 });
